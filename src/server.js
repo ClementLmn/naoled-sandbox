3fae858f493279d6be1969299df6ae9455405e79
Clément Lemoine <clementlemoine.cl@gmail.com>
2018-11-27 12:51:43 +0100

    change server port

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.port || 3333;
+const port = process.env.port || 3000;
+
+app.use(express.json());
 
 app.post('/api', async (req, res) => {
   console.log(req.body);
