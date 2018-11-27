59ee2a4bc7c2652ff61831292c76ee1f565c46e7
guillaume <guillaume-duclos@hotmail.fr>
2018-11-27 16:53:34 +0100

    Mise en place du capteur de luminosité

--- hardware/lumiere-chauffage/src/main.ino
+++ hardware/lumiere-chauffage/src/main.ino
@@ -2,9 +2,12 @@
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
 
+int photocellPin = 0;
+int photocellReading;
+
 void setup() {
 	Serial.begin(115200);
-	WiFi.begin("Honor 5C", "azertyuiop");
+	WiFi.begin("iPhone de Guillaume", "commentestvotreblanquette?");
 	while (WiFi.status() != WL_CONNECTED) {
 		Serial.println(".");
 		delay(500);
@@ -13,15 +16,15 @@ void setup() {
 
 void loop() {
 	if (WiFi.status() == WL_CONNECTED) {
+		photocellReading = analogRead(photocellPin);
 		StaticJsonBuffer<300> JSONbuffer;
 		JsonObject& JSONencoder = JSONbuffer.createObject();
-		JSONencoder["name"] = "test";
-		JSONencoder["value"] = random(100);
+		JSONencoder["value"] = photocellReading;
 		char JSONmessageBuffer[300];
 		JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
 		Serial.println(JSONmessageBuffer);
 		HTTPClient http;
-		http.begin("http://192.168.43.149:3000/api");
+		http.begin("http://172.20.10.3:3000/api");
 		http.addHeader("Content-Type", "application/json");
 		int httpCode = http.POST(JSONmessageBuffer);
 		String payload = http.getString();
@@ -29,5 +32,5 @@ void loop() {
 		Serial.println(payload);
 		http.end();
 	}
-	delay(5000);
+	delay(250);
 }
