57f4160d0c6b23aee845a7f9059eb505e846abce
Thomas <thomas.guillet35@hotmail.fr>
2018-11-27 12:52:49 +0100

    :collision: init lumieres

---
+++ hardware/lumiere-chauffage/src/main.ino
@@ -0,0 +1,33 @@
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+
+void setup() {
+	Serial.begin(115200);
+	WiFi.begin("Honor 5C", "azertyuiop");
+	while (WiFi.status() != WL_CONNECTED) {
+		Serial.println(".");
+		delay(500);
+	}
+}
+
+void loop() {
+	if (WiFi.status() == WL_CONNECTED) {
+		StaticJsonBuffer<300> JSONbuffer;
+		JsonObject& JSONencoder = JSONbuffer.createObject();
+		JSONencoder["name"] = "test";
+		JSONencoder["value"] = random(100);
+		char JSONmessageBuffer[300];
+		JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
+		Serial.println(JSONmessageBuffer);
+		HTTPClient http;
+		http.begin("http://192.168.43.149:3000/api");
+		http.addHeader("Content-Type", "application/json");
+		int httpCode = http.POST(JSONmessageBuffer);
+		String payload = http.getString();
+		Serial.println(httpCode);
+		Serial.println(payload);
+		http.end();
+	}
+	delay(5000);
+}
