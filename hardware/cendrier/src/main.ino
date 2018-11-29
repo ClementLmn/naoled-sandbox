a72994b667dd1fe83b0e4bb74080b1a417ee3c22
Thomas <thomas.guillet35@hotmail.fr>
2018-11-29 10:12:24 +0100

    Button works :hankey:

--- hardware/cendrier/src/main.ino
+++ hardware/cendrier/src/main.ino
@@ -1,12 +1,25 @@
 #include <Button.h>
 
-Button button = Button(13,INPUT_PULLUP);
+// NAME : EYJAFJALLAJOKULL
+
+Button button = Button(D4, INPUT_PULLUP);
 
 void setup() {
 	Serial.begin(115200);
+  pinMode(D5, OUTPUT);
 }
 
 void loop() {
-  Serial.println(button.isPressed());
-  delay(500);
+  if(button.uniquePress()){
+    // +1 cigarette mise au cendrier
+    Serial.println("pressed");
+    // On envoi
+    goLight();
+  }
+}
+
+void goLight() {
+  digitalWrite(D5, HIGH);
+  delay(1000);
+  digitalWrite(D5, LOW);
 }
