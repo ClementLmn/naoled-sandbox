9c08cef72c908e685e6c99b47654540e02f6720d
guillaume <guillaume-duclos@hotmail.fr>
2018-11-30 18:34:48 +0100

    Un début du branchement vers l'API

--- hardware/escalier/src/main.ino
+++ hardware/escalier/src/main.ino
@@ -1,17 +1,24 @@
+#define FASTLED_INTERRUPT_RETRY_COUNT 0
+#define FASTLED_ESP8266_D1_PIN_ORDER
+#include <FastLED.h>
 #include <Button.h>
 #include <Adafruit_NeoPixel.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+// #include <ArduinoJson.h>
 
 // NAME : STANNAH
 
-#define DEBUG 0
-#define PIN_STRIP_1 D10
+
+
+#define DEBUG 1
+#define PIN_STRIP_1 D2
 #define PIN_STRIP_2 D6
-#define NUM_LED 8
 
 #if DEBUG
 #define NUM_STRIPS 2
 #else
-#define PIN_STRIP_3 D2
+#define PIN_STRIP_3 D10
 #define NUM_STRIPS 3
 #endif
 
@@ -24,6 +31,12 @@ Button button = Button(D3, INPUT_PULLUP);
 
 void setup() {
 	Serial.begin(115200);
+  WiFi.begin("Honor 5C", "azertyuiop");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println(".");
+    delay(500);
+  }
+
 	// tell FastLED there's 60 NEOPIXEL leds on pin 10, starting at index 0 in the led array
 	FastLED.addLeds<NEOPIXEL, PIN_STRIP_1>(leds, 0, NUM_LEDS_PER_STRIP);
 	// tell FastLED there's 60 NEOPIXEL leds on pin 11, starting at index 60 in the led array
@@ -32,24 +45,38 @@ void setup() {
 	// tell FastLED there's 60 NEOPIXEL leds on pin 12, starting at index 120 in the led array
 	FastLED.addLeds<NEOPIXEL, PIN_STRIP_3>(leds, 2 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
 	#endif
+  delay(200);
 }
 
 void loop() {
+  Serial.println("mdr");
 	if (button.uniquePress()) {
-    anim(60, 8, CRGB(0, 199, 183));
-    anim(60, 8, CRGB(0, 0, 0));
+    anim(CRGB(0, 199, 183));
+    Serial.println("LOL");
+    anim(CRGB(0, 0, 0));
+    HTTPClient http;
+    http.begin("http://api-naoled.cleverapps.io/STAIRS");
+    http.addHeader("Content-Type", "application/json");
+    int httpCode = http.POST("{\"name\":\"STANNAH\"}");
+    Serial.println(httpCode);
+
+    // StaticJsonBuffer<300> JSONbuffer;
+    // JsonObject& listData = JSONbuffer.createObject();
+    // listData["name"] = "STANNAH";
+    // sendData(listData);
 	}
+  delay(50);
 }
 
-void anim(uint8_t num_led, uint8_t wave_size, CRGB led_color) {
+void anim(CRGB led_color) {
 
   uint8_t center_1 = 29;
   uint8_t center_2 = 89;
   uint8_t center_3 = 149;
 
-  uint16_t i, j;
+  uint16_t i;
 
-  for (i = 0; i < NUM_LEDS_PER_STRIP + wave_size / 2; i++) {
+  for (i = 0; i < NUM_LEDS_PER_STRIP; i++) {
     if (i < 15) {
       leds[center_1 + i] = led_color;
       leds[center_1 - i] = led_color;
@@ -71,11 +98,34 @@ void anim(uint8_t num_led, uint8_t wave_size, CRGB led_color) {
 
     delay(10);
   }
-
-  for(j=0; j<wave_size; j++) {
-    strip.setPixelColor(j + 1, strip.Color(0, 0, 0));
-    strip.setPixelColor(total-j+1, strip.Color(0, 0, 0));
-    strip.show();
-  }
-  delay(wait);
 }
+
+// void sendData(JsonObject& listData) {
+//   if (WiFi.status() == WL_CONNECTED) {
+//     //Declare object of class HTTPClient
+//     HTTPClient http;
+//     // Prettier data
+//     char JSONmessageBuffer[300];
+//     listData.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
+//     // connect api
+//     http.begin("http://api-naoled.cleverapps.io/STAIRS");
+//     http.addHeader("Content-Type", "application/json");
+//     // send data
+//     int httpCode = http.POST(JSONmessageBuffer);
+//     // <0 if error
+//     if(httpCode > 0){
+//       Serial.printf("[HTTP] POST... code: %d\n", httpCode);
+//       String payload = http.getString();
+//       // HTTP return code
+//       Serial.println(httpCode);
+//       // request response payload
+//       Serial.println(payload);
+//     }else{
+//       Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+//     }
+//     http.end();
+//     Serial.print("Message envoyé");
+//   }else{
+//     Serial.printf("[HTTP} Internet not found\n");
+//   }
+// }
